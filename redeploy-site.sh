7d923ad40562cd524d3f9ee064ad4eafc7250b5f
rysonw <rysonplays@gmail.com>
2022-07-28 10:56:09 -1000

    Small Changes

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/flask-portfolio
+cd flask-portfolio
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
